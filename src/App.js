037ffd0ee3be9e89cb66a99e9a6a637bd552492b
50HJ <107606781+50HJ@users.noreply.github.com>
2022-10-29 13:58:22 +0000

    ANDROID: exec_domains: Disable request_module() call for personalities

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+import Header from './components/Header'
+import MainContainer from './containers/MainContainer'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <MainContainer/>
+      </div>
+    );
+  }
