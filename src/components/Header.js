8caff8fc8db5739ac8dbfb7f6e788e3d43192ead
50HJ <107606781+50HJ@users.noreply.github.com>
2022-11-05 18:26:04 +0000

    Fixed styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,3 +5,12 @@ class Header extends Component {
 
   render() {
     return (
+      <header>
+        <h1 className="text-center">Flatiron Stock Exchange</h1>
+      </header>
+    );
+  }
+
+}
+
+export default Header;
