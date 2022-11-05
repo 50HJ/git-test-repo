8fe22b903c41f5b2c6af00cac9c0167844a717bb
50HJ <107606781+50HJ@users.noreply.github.com>
2022-11-05 18:52:38 +0000

    Add shared scheme

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,3 +2,15 @@
 import React from 'react';
 
 const SearchBar = () => {
+  return (
+    <div>
+
+      <strong>Sort by:</strong>
+      <label>
+        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        Alphabetically
+      </label>
+      <label>
+        <input type="radio" value="Price" checked={null} onChange={null}/>
+        Price
+      </label>
