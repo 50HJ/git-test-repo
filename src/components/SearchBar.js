19ebd5c1864e6220106bc8d35346f2758005fd53
50HJ <107606781+50HJ@users.noreply.github.com>
2022-11-05 19:26:49 +0000

    Build enhancement

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,3 +14,16 @@ const SearchBar = () => {
         <input type="radio" value="Price" checked={null} onChange={null}/>
         Price
       </label>
+      <br/>
+
+      <label>
+        <strong>Filter:</strong>
+        <select onChange={null}>
+          <option value="Tech">Tech</option>
+          <option value="Sportswear">Sportswear</option>
+          <option value="Finance">Finance</option>
+        </select>
+      </label>
+
+
+    </div>
