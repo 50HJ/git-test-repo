30d292425aaf09442f9b470acf11394b4931ba09
50HJ <107606781+50HJ@users.noreply.github.com>
2023-01-01 16:11:21 +0000

    Update development and testing dependencies

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -1,2 +1,18 @@
 
 import React, { Component } from 'react';
+import Stock from '../components/Stock'
+
+class StockContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Stocks</h2>
+        {
+          //render the list of stocks here
+        }
+      </div>
+    );
+  }
+
+}
