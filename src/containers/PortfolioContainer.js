703a52c6b7bbddd750128facef895a8c66b878e2
50HJ <107606781+50HJ@users.noreply.github.com>
2023-01-01 11:59:03 +0000

    Add CHANGELOG.md.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,3 +3,18 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>My Portfolio</h2>
+          {
+            //render your portfolio stocks here
+          }
+      </div>
+    );
+  }
+
+}
+
+export default PortfolioContainer;
